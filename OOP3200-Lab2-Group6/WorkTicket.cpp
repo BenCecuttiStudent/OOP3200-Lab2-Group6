52961200163df13249fd0a8d978406cfa09b2bd5
Benjamin Cecutti <benjamin.cecutti@dcmail.ca>
2021-09-24 18:17:38 -0400

    Added operator overload ==, and created ToString function

--- OOP3200-Lab2-Group6/WorkTicket.cpp
+++ OOP3200-Lab2-Group6/WorkTicket.cpp
@@ -8,139 +8,155 @@
 #include <iostream>
 #include <stdexcept>
 
-class WorkTicket
+
+// Constructor
+WorkTicket::WorkTicket(int work_ticket_number, const std::string& client_id, int work_ticket_day, int work_ticket_month,
+                       int work_ticket_year, const std::string& work_ticket_description)
 {
-private:
-	// Attributes
-	int workTicketNumber = 0;
-	std::string clientID = " ";
-	int workTicketDay = 0;
-	int workTicketMonth = 0;
-	int workTicketYear = 0;
-	std::string workTicketDescription = " ";
+	SetWorkTicket(work_ticket_number, client_id, work_ticket_day, work_ticket_month, work_ticket_year,
+	              work_ticket_description);
+}
 
-public:
-	// Parameterized Constructor
-	WorkTicket(const int work_ticket_number, const std::string& client_id, const int work_ticket_day,
-		const int work_ticket_month, const int work_ticket_year, const std::string& work_ticket_description)
+WorkTicket::~WorkTicket() = default;
+
+
+// Copy Constructor
+WorkTicket::WorkTicket(const WorkTicket& ticket2)
+{
+	SetWorkTicket(ticket2.GetWorkTicketNumber(), ticket2.GetClientID(), ticket2.GetWorkTicketDay(),
+	              ticket2.GetWorkTicketMonth(), ticket2.GetWorkTicketYear(), ticket2.GetWorkTicketDescription());
+	std::cout << "\nA WorkTicket object was COPIED. \n";
+}
+
+// Getters
+int WorkTicket::GetWorkTicketNumber() const { return workTicketNumber; }
+
+std::string WorkTicket::GetClientID() const { return clientID; }
+
+std::string WorkTicket::GetWorkTicketDescription() const { return workTicketDescription; }
+
+int WorkTicket::GetWorkTicketDay() const { return workTicketDay; }
+
+int WorkTicket::GetWorkTicketMonth() const { return workTicketMonth; }
+
+int WorkTicket::GetWorkTicketYear() const { return workTicketYear; }
+
+// Setters
+void WorkTicket::SetWorkTicketNumber(int work_ticket_number) { workTicketNumber = work_ticket_number; }
+
+void WorkTicket::SetClientID(const std::string& client_id) { clientID = client_id; }
+
+void WorkTicket::SetWorkTicketDay(const int work_ticket_day) { workTicketDay = work_ticket_day; }
+
+void WorkTicket::SetWorkTicketMonth(const int work_ticket_month) { workTicketMonth = work_ticket_month; }
+
+void WorkTicket::SetWorkTicketYear(const int work_ticket_year) { workTicketYear = work_ticket_year; }
+
+void WorkTicket::SetWorkTicketDescription(const std::string& work_ticket_description)
+{
+	workTicketDescription = work_ticket_description;
+}
+
+bool WorkTicket::SetWorkTicket(const int work_ticket_number, const std::string& client_id, const int work_ticket_day, const int work_ticket_month, const int work_ticket_year, const std::string& work_ticket_description)
+{
+	try
 	{
-		workTicketNumber = work_ticket_number;
-		clientID = client_id;
-		workTicketDay = work_ticket_day;
-		workTicketMonth = work_ticket_month;
-		workTicketYear = work_ticket_year;
-		workTicketDescription = work_ticket_description;
-	}
-
-	// Default Constructor
-	WorkTicket()
-	{
-		workTicketNumber = 0;
-		clientID = "";
-		workTicketDay = 1;
-		workTicketMonth = 1;
-		workTicketYear = 2000;
-		workTicketDescription = "";
-	}
-
-	// Getters
-	int GetWorkTicketNumber() const { return workTicketNumber; }
-
-	std::string GetClientID() const { return clientID; }
-
-	std::string GetWorkTicketDescription() const { return workTicketDescription; }
-
-	int GetWorkTicketDay() const { return workTicketDay; }
-
-	int GetWorkTicketMonth() const { return workTicketMonth; }
-
-	int GetWorkTicketYear() const { return workTicketYear; }
-
-	// Setters
-	void SetWorkTicketNumber(int work_ticket_number) { workTicketNumber = work_ticket_number; }
-
-	void SetClientID(const std::string& client_id) { clientID = client_id; }
-
-	void SetWorkTicketDay(const int work_ticket_day) { workTicketDay = work_ticket_day; }
-
-	void SetWorkTicketMonth(const int work_ticket_month) { workTicketMonth = work_ticket_month; }
-
-	void SetWorkTicketYear(const int work_ticket_year) { workTicketYear = work_ticket_year; }
-
-	void SetWorkTicketDescription(const std::string& work_ticket_description)
-	{
-		workTicketDescription = work_ticket_description;
-	}
-
-	bool SetWorkTicket(const int work_ticket_number, const std::string& client_id, const int work_ticket_day, const int work_ticket_month, const int work_ticket_year, const std::string& work_ticket_description)
-	{
-
-		try
+		if (work_ticket_number > 0)
 		{
-			if (work_ticket_number > 0)
-			{
-				SetWorkTicketNumber(work_ticket_number);
-			}
-			else
-			{
-				throw std::invalid_argument("Number must be greater or equal to 0");
-			}
-
-			if (client_id.length() > 0)
-			{
-				SetClientID(client_id);
-			}
-			else
-			{
-				throw std::invalid_argument("String must be at least one character");
-			}
-
-			if (work_ticket_day >= 1 && work_ticket_day <= 31)
-			{
-				SetWorkTicketDay(work_ticket_day);
-			}
-			else
-			{
-				throw std::invalid_argument("Number must be between 1 and 31");
-			}
-
-			if (work_ticket_month >= 1 && work_ticket_month <= 12)
-			{
-				SetWorkTicketMonth(work_ticket_month);
-			}
-			else
-			{
-				throw std::invalid_argument("Number must be between 1 and 12");
-			}
-
-			if (work_ticket_year >= 2000 && work_ticket_year <= 2099)
-			{
-				SetWorkTicketYear(work_ticket_year);
-			}
-			else
-			{
-				throw std::invalid_argument("Number must be between 2000 and 2099");
-			}
-			return true;
+			SetWorkTicketNumber(work_ticket_number);
 		}
-		catch (std::exception ex)
+		else
 		{
-			std::cout << ex.what();
-			return false;
+			throw std::invalid_argument("Number must be greater or equal to 0");
 		}
 
-	}
+		if (client_id.length() > 0)
+		{
+			SetClientID(client_id);
+		}
+		else
+		{
+			throw std::invalid_argument("String must be at least one character");
+		}
 
-	// Output Ticket Information
-	void ShowWorkTicket()
-	{
-		std::cout << "Number: ";
-		std::cout << GetWorkTicketNumber() << std::endl;
-		std::cout << "ClientID: ";
-		std::cout << GetClientID() << std::endl;
-		std::cout << "Date: ";
-		std::cout << GetWorkTicketDay() << "/" << GetWorkTicketMonth() << "/" << GetWorkTicketYear() << std::endl;
-		std::cout << "Description: ";
-		std::cout << GetWorkTicketDescription() << std::endl;
+		if (work_ticket_day >= 1 && work_ticket_day <= 31)
+		{
+			SetWorkTicketDay(work_ticket_day);
+		}
+		else
+		{
+			throw std::invalid_argument("Number must be between 1 and 31");
+		}
+
+		if (work_ticket_month >= 1 && work_ticket_month <= 12)
+		{
+			SetWorkTicketMonth(work_ticket_month);
+		}
+		else
+		{
+			throw std::invalid_argument("Number must be between 1 and 12");
+		}
+
+		if (work_ticket_year >= 2000 && work_ticket_year <= 2099)
+		{
+			SetWorkTicketYear(work_ticket_year);
+		}
+		else
+		{
+			throw std::invalid_argument("Number must be between 2000 and 2099");
+		}
+		return true;
 	}
-};
+	catch (std::exception& ex)
+	{
+		std::cout << ex.what();
+		return false;
+	}
+}
+
+bool WorkTicket::operator==(const WorkTicket& newTicket) const
+{
+	return (GetWorkTicketNumber() == newTicket.GetWorkTicketNumber())
+			&& (GetClientID() == newTicket.GetClientID())
+			&& (GetWorkTicketDay() == newTicket.GetWorkTicketDay())
+			&& (GetWorkTicketMonth() == newTicket.GetWorkTicketMonth())
+			&& (GetWorkTicketYear() == newTicket.GetWorkTicketYear())
+			&& (GetWorkTicketDescription() == newTicket.GetWorkTicketDescription());
+}
+
+// Output Ticket Information
+void WorkTicket::ShowWorkTicket() const
+{
+	std::cout << "Number: ";
+	std::cout << GetWorkTicketNumber() << std::endl;
+	std::cout << "ClientID: ";
+	std::cout << GetClientID() << std::endl;
+	std::cout << "Date: ";
+	std::cout << GetWorkTicketDay() << "/" << GetWorkTicketMonth() << "/" << GetWorkTicketYear() << std::endl;
+	std::cout << "Description: ";
+	std::cout << GetWorkTicketDescription() << std::endl;
+}
+
+std::string WorkTicket::ToString() const
+{
+	std::string temp;
+
+	temp.append("WorkTicket # ");
+	temp.append(std::to_string(GetWorkTicketNumber()));
+	temp.append(" - ");
+	temp.append(GetClientID());
+	temp.append(" (");
+	temp.append(std::to_string(GetWorkTicketDay()));
+	temp.append("/");
+	temp.append(std::to_string(GetWorkTicketMonth()));
+	temp.append("/");
+	temp.append(std::to_string(GetWorkTicketYear()));
+	temp.append("): ");
+	temp.append(GetWorkTicketDescription());
+	temp.append("\n");
+
+	std::cout << temp;
+	return temp;
+
+	// Work Ticket # 2 - ABC123 (10/3/1012): User cannot locate 'any' key
+}
--- OOP3200-Lab2-Group6/WorkTicket.h
+++ OOP3200-Lab2-Group6/WorkTicket.h
@@ -1,1 +1,66 @@
 #pragma once
+#ifndef WORK_TICKET_
+#define WORK_TICKET_
+#include <string>
+
+class WorkTicket
+{
+
+
+public:
+
+	/* Constructor */
+	WorkTicket(int work_ticket_number = 0, const std::string& client_id = "", int work_ticket_day = 0, int work_ticket_month = 0, int work_ticket_year = 0,
+	           const std::string& work_ticket_description = "");
+
+	/* Deconstructor */
+	~WorkTicket();
+
+	/* Copy Constructor */
+	WorkTicket(const WorkTicket& ticket2);
+	
+	/* Accessors */
+	int GetWorkTicketNumber() const;
+
+	std::string GetClientID() const;
+
+	std::string GetWorkTicketDescription() const;
+
+	int GetWorkTicketDay() const;
+
+	int GetWorkTicketMonth() const;
+
+	int GetWorkTicketYear() const;
+
+	/* Mutators */
+	void SetWorkTicketNumber(int work_ticket_number);
+
+	void SetClientID(const std::string& client_id);
+
+	void SetWorkTicketDay(const int work_ticket_day);
+
+	void SetWorkTicketMonth(const int work_ticket_month);
+
+	void SetWorkTicketYear(const int work_ticket_year);
+
+	void SetWorkTicketDescription(const std::string& work_ticket_description);
+
+	bool SetWorkTicket(const int work_ticket_number, const std::string& client_id, const int work_ticket_day, const int work_ticket_month, const int work_ticket_year, const std::string& work_ticket_description);
+	
+	/* Operator Overloads */
+	bool operator== (const WorkTicket& newTicket) const;
+
+	/* Utility */
+	void ShowWorkTicket() const;
+
+	std::string ToString() const;
+
+private:
+	int workTicketNumber{};
+	std::string clientID;
+	int workTicketDay{};
+	int workTicketMonth{};
+	int workTicketYear{};
+	std::string workTicketDescription;
+};
+#endif
